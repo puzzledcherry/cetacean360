9920672c42376fde33f030d803bf25c0f67fc15e
skylatran <skylatran03@gmail.com>
2023-12-21 12:02:33 -0800

    python

--- jsonToGeo.py
+++ jsonToGeo.py
@@ -4,21 +4,29 @@ import simplejson as json
 
 script, in_file, out_file = argv
 
-data = json.load(open(in_file))
+import urllib.request, json 
+with urllib.request.urlopen("https://acartia.io/api/v1/sightings/current") as url:
+    data = json.load(url)
 
 geojson = {
     "type": "FeatureCollection",
-    "features": [
+    "features": [ 
     {
         "type": "Feature",
         "geometry" : {
             "type": "Point",
             "coordinates": [d["longitude"], d["latitude"]],
             },
-        "properties" : d,
+        "properties" : {
+            "data_source_name": d["data_source_name"],
+            "data_source_entity": d["data_source_entity"],
+            "created": d["created"],
+            "no_sighted": d["no_sighted"],
+            "type": d["type"],
+            "data_source_comments": d["data_source_comments"], 
+        }
      } for d in data]
 }
-
 
 output = open(out_file, 'w')
 json.dump(geojson, output)
