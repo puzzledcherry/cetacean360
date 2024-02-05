10c776e7e5757c715a173e949cce29067162ecdb
skylatran <skylatran03@gmail.com>
2024-02-05 10:36:14 -0800

    scraper?

--- data/scraper.py
+++ data/scraper.py
@@ -4,23 +4,26 @@
 # description:
 
 import os
-import re
 import requests
 import pandas as pd
 
+# method def
 def scrape(acartia_path='./data/'):
   # acartia token
   token = os.environ['ACARTIA_TOKEN_HERE']
   
-  # acartia API call
+  # acartia API call, provide token and get JSON back
   url='https://acartia.io/api/v1/sightings/'
   response = requests.get(url, headers={'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json'})
   
   # saving to pandas dataframe (basically a table)
   acartia = pd.DataFrame(response.json(), 
-                     columns=['type','created','trusted','entry_id','latitude','longitude','photo_url',
-                              'ssemmi_id','no_sighted','data_source_witness', 'data_source_comments'])
+                      columns=['type','created','profile','trusted','entry_id','latitude','longitude','photo_url','signature',
+                              'ssemmi_id','no_sighted','submitter_did','data_source_id',
+                              'data_source_name','ssemmi_date_added','data_source_entity', 'data_source_witness', 'data_source_comments'])
   
+  # cleaning the table
+  # reducing columns, dropping duplicates, sorting by newest
   acartia = acartia[['type','created','latitude','longitude','no_sighted','data_source_id','data_source_comments']]
   acartia = acartia.drop_duplicates()
   acartia = acartia.sort_values(by=['created'])
@@ -28,5 +31,6 @@ def scrape(acartia_path='./data/'):
   # acartia to csv
   acartia.to_csv(acartia_path+'acartia.csv', index=False)
 
+# method call
 scrape()
 
