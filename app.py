8bb37b492fd7381074a139cb1f9c3812bbf3a7ac
skylatran <skylatran03@gmail.com>
2024-02-13 13:36:22 -0800

    reformatting map and OOP

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import plotly.express as px
 import sys
 sys.path.insert(0, 'data')
 from hidden import MAPBOX_TOKEN
+px.set_mapbox_access_token(MAPBOX_TOKEN)
 
 # create dash application 
 app = dash.Dash(
@@ -27,33 +28,34 @@ app = dash.Dash(
 server = app.server
 
 
+# loading csv file into a pandas dataframe object
+def readCSV(csvFilePath):
+    try:
+        dataFrameObject = pd.read_csv(csvFilePath)
+        print("CSV file loaded successfully.")
+    except FileNotFoundError:
+        print("Error: CSV file not found.")
+    except Exception as e:
+        print("An error occurred while loading the CSV file:", e)
+    return dataFrameObject
 
-# !! add methods here as needed
-
-px.set_mapbox_access_token(MAPBOX_TOKEN)
-# loading csv file
-try:
-    whaleSpottings = pd.read_csv(r"data/acartiaDataPull.csv")
-    print("CSV file loaded successfully.")
-except FileNotFoundError:
-    print("Error: CSV file not found.")
-except Exception as e:
-    print("An error occurred while loading the CSV file:", e)
-    
-# creating figure    
-fig = px.scatter_mapbox(whaleSpottings, lat="latitude", lon="longitude", hover_name="type", 
-                        hover_data=["data_source_id", "created", "no_sighted", "data_source_comments"],
-                        color_discrete_sequence=["blue"], zoom = 3, height = 300)
-fig.update_layout(mapbox_style="streets")
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.show()
-
+# creating whale map figure 
+def createWhaleMap():  
+    whaleSpottings = readCSV(r"data/acartiaDataPull.csv") 
+    fig = px.scatter_mapbox(whaleSpottings, lat="latitude", lon="longitude", hover_name="type", 
+                            hover_data=["data_source_id", "created", "no_sighted", "data_source_comments"],
+                            color_discrete_sequence=["blue"], zoom = 3, height = 900, width = 900)
+    fig.update_layout(mapbox_style="streets")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+# calling function
+whaleMapFig = createWhaleMap()
 
 # start designing the actual website
 app.layout = html.Div([
     dcc.Graph(
         id = 'whale-map',
-        figure = fig  # Assign fig to dcc.Graph
+        figure = whaleMapFig  # Assign fig to dcc.Graph
     )
 ])
 
