9a0d196dc4e87de9cf86162169f20d226cbc1474
skylatran <skylatran03@gmail.com>
2024-02-22 13:51:48 -0800

    css styling & more html

--- app.py
+++ app.py
@@ -6,6 +6,7 @@
 import dash
 from dash import html
 from dash import dcc
+import dash_bootstrap_components as dbc
 
 import pandas as pd
 import plotly.express as px
@@ -16,17 +17,9 @@ from hidden import MAPBOX_TOKEN
 px.set_mapbox_access_token(MAPBOX_TOKEN)
 
 # create dash application 
-app = dash.Dash(
-    __name__,
-    meta_tags = [
-        {"name": "viewport", "content": "width=device-width, initial-scale=1.0"},
-        {"name": "description", "content": "Whale Sighting Dashboard"}, 
-        {"name": "news_keywords", "content": "Whale, Orca, Killer Whale, Puget Sound, Marine Exchange of Puget Sound, MAREXPS"}
-        ],
-    )
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
  # accessing flask server attribute through server variable
 server = app.server
-
 
 # loading csv file into a pandas dataframe object
 def readCSV(csvFilePath):
@@ -44,7 +37,7 @@ def createWhaleMap():
     whaleSpottings = readCSV(r"data/acartiaDataPull.csv") 
     fig = px.scatter_mapbox(whaleSpottings, lat = "latitude", lon = "longitude", hover_name = "type", 
                             hover_data=["data_source_id", "trusted", "created", "no_sighted", "data_source_comments"],
-                            color_discrete_sequence=["blue"], zoom = 3, height = 900, width = 900)
+                            color_discrete_sequence=["blue"], zoom = 7, center = {"lat": 48.1418, "lon":-122.4244})
     fig.update_layout(mapbox_style="streets")
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig
@@ -53,22 +46,28 @@ whaleMapFig = createWhaleMap()
 
 # start designing the actual website
 app.layout = html.Div(
-    # components within the webapp as a whole
+    className = "the-whole-website",
     children = [
         html.Div(
             className = "map-and-intro-container",
             children = [
                 html.Div(
-                    id = "tite-container"
+                    className = "title-container",
+                    children = [
+                        html.H1("SEVEN DAYS"),
+                        html.H1("OF"), 
+                        html.H1("WHALE"),
+                        html.H1("SIGHTINGS") 
+                    ]
                 ),
                 html.Div(
-                    id = "map-container",
+                    className = "map-container",
                     children = [
                         # whale map
                         dcc.Graph(
-                            id = 'whale-map',
-                            figure = whaleMapFig  # Assign fig to dcc.Graph
-                        )
+                            className = 'whale-map',
+                            figure = whaleMapFig,  # Assign fig to dcc.Graph
+                        )  
                     ]
                 )
             ]
