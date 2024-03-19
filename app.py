ed3f6731ab97ba71929309741b729428fb6ffdb0
skylatran <skylatran03@gmail.com>
2024-03-19 11:11:02 -0700

    hover text for sightings

--- app.py
+++ app.py
@@ -3,15 +3,18 @@
 # program name: app.py
 # description:
 
+# dash framework imports
 import dash
 from dash import html
 from dash import dcc
 import dash_bootstrap_components as dbc
 
+# panda data frame imports
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
 
+# include data subfolder to path, API tokens
 import sys
 sys.path.insert(0, 'data')
 from hidden import MAPBOX_TOKEN
@@ -19,7 +22,6 @@ px.set_mapbox_access_token(MAPBOX_TOKEN)
 
 # create dash application 
 app = dash.Dash(__name__)
-# accessing flask server attribute through server variable
 server = app.server
 
 # loading csv file into a pandas dataframe object
@@ -32,6 +34,21 @@ def readCSV(csvFilePath):
     except Exception as e:
         raise Exception("An error occurred while loading the CSV file:", e)
     return dataFrameObject
+
+# limit the width of a line in the hover text
+def limitLineWidth(line, max_width = 50):
+    # base case
+    # if the line is over the max width, find space near max
+    if len(line) > max_width:
+        last_space_index = line[:max_width].rfind(' ')
+        # if there is a space, break there and recursive call
+        if last_space_index != -1:
+            return line[:last_space_index] + '<br>' + limitLineWidth(line[last_space_index+1:], max_width)
+        # else, break at max then recursive call
+        else:
+            return line[:max_width] + '<br>' + limitLineWidth(line[max_width:], max_width)
+    else:
+        return line
 
 # create map with lines connecting whale sightings
 def createMap():
@@ -65,29 +82,42 @@ def createMap():
                     mode = 'lines',
                     lon = [current_row['lon'], next_row['lon']],
                     lat = [current_row['lat'], next_row['lat']],
-                    line = dict(width = 1,color = 'red'),
+                    line = dict(width = 1,color = 'purple'),
                 )
             )
-        # otherise just continue
-        else:
-            continue
+    
+    # create text for each row of the acartia data frame
+    # used as hover text
+    hover_text = acartiaDF.apply(lambda row: 
+        f"{limitLineWidth(row['type'])}<br>"
+        f"{limitLineWidth('Count: ' + str(row['no_sighted']))}<br>"
+        f"{limitLineWidth('Created: ' + row['created'])}<br>"
+        f"<br>"
+        f"{limitLineWidth('Comments: ' + str(row['data_source_comments']))}",
+        axis=1)
     
     # add dots for each sighting on the map
     # include hover info
     fig.add_trace(
         go.Scattermapbox(
             mode='markers',
-            lon = connectedDF['lon'],
-            lat = connectedDF['lat'],
-            marker = dict(size=8, color='blue', opacity=0.7),
-            hoverinfo ='text',
-            text = connectedDF['comment']  # Display 'id' on hover
+            lon = acartiaDF['longitude'],
+            lat = acartiaDF['latitude'],
+            marker = dict(size = 8, color = 'blue', opacity = 0.7),
+            
+            hoverinfo = 'text',
+            text = hover_text,
+            
+            hoverlabel = dict (
+                bgcolor = 'blue',
+                align = 'left',
+            )
         )
     )
     
     return fig
 
-# display the map
+# HTML, display the map
 app.layout = html.Div(
     className = 'whole-website',
     children = [
